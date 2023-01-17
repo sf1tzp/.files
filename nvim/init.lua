cb4a26817f754e382cf8a4cb362073abbbe7d168
Steven Fitzpatrick <steven@fitz.gg>
2023-01-17 11:11:31 -0600

    Tweak load order for successful bootstrap

--- nvim/init.lua
+++ nvim/init.lua
@@ -1,12 +1,12 @@
 if vim.g.vscode then
   print("Using empty init.lua for VS Code Extension")
 else
-  require("options")
   require("plugins")
   require("lsp")
   require("setup")
 
-  -- define keymaps after plugin setup
+  -- customize after plugin setup
+  require("options")
   require("keymap")
 end
 
