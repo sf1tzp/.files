30fbff83094389c0fe7ca9f5d9457f0ec1ccf865
Steven Fitzpatrick <steven@fitz.gg>
2023-01-17 21:14:43 -0600

    Update lsp hover keymap

--- nvim/lua/lsp.lua
+++ nvim/lua/lsp.lua
@@ -52,7 +52,7 @@ lsp.on_attach(function(client, bufnr)
 
   vim.keymap.set("n", "gd", function() vim.lsp.buf.definition() end, opts)
   vim.keymap.set("n", "gt", function() vim.lsp.buf.type_definition() end, opts)
-  vim.keymap.set("n", "K", function() vim.lsp.buf.hover() end, opts)
+  vim.keymap.set("n", "<leader>k", function() vim.lsp.buf.hover() end, opts)
   vim.keymap.set("n", "<leader>vws", function() vim.lsp.buf.workspace_symbol() end, opts)
   vim.keymap.set("n", "<leader>vd", function() vim.diagnostic.open_float() end, opts)
   vim.keymap.set("n", "[d", function() vim.diagnostic.goto_next() end, opts)
