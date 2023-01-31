0e112a52913d86c10caf618e0f3db9fd2916f908
Steven Fitzpatrick <steven@dev-vm.w1tiyzgs4dvu1ghargorpbyvig.jx.internal.cloudapp.net>
2023-01-31 16:17:40 +0000

    Configure autosave

--- nvim/lua/setup.lua
+++ nvim/lua/setup.lua
@@ -120,3 +120,13 @@ require("obsidian").setup({
   }
 })
 
+
+-- autosave when leaving buffers
+vim.api.nvim_create_autocmd({ "BufLeave", "FocusLost" }, {
+  callback = function()
+    if vim.bo.modified and not vim.bo.readonly and vim.fn.expand("%") ~= "" and vim.bo.buftype == "" then
+      vim.api.nvim_command('silent update')
+    end
+  end,
+})
+
