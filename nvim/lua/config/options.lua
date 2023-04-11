062ae1a2317225051aa385f5a6539ed9b82d9894
Steven Fitzpatrick <steven@fitz.gg>
2023-04-11 04:11:40 -0500

    Migrate git blame plugin

--- nvim/lua/config/options.lua
+++ nvim/lua/config/options.lua
@@ -2,6 +2,14 @@
 -- Default options that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/options.lua
 -- Add any additional options here
 
+-- code rulers
 vim.opt.colorcolumn = { "80", "120" }
+
+-- whitespace characters
 vim.opt.list = false
 vim.g.strip_whitespace_on_save = 1
+
+-- git blame
+vim.g.gitblame_enabled = 0 -- don't render by default
+vim.g.gitblame_date_format = '%r'
+vim.g.gitblame_message_template = '\t\t\t\t\t<author>, <date> <sha> <summary>'
---
+++ nvim/lua/plugins/blame.lua
@@ -0,0 +1,6 @@
+return {
+  "f-person/git-blame.nvim",
+  keys = {
+    { "<leader>??", "<cmd>GitBlameToggle<cr>", desc = "Toggle git blame" },
+  },
+}
