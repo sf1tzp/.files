82e8efa3511a32d89d72366c842fa92a36a376d7
Steven Fitzpatrick <steven@fitz.gg>
2023-04-11 11:59:22 -0500

    Update git blame keys

--- nvim/lua/plugins/blame.lua
+++ nvim/lua/plugins/blame.lua
@@ -1,6 +1,7 @@
 return {
   "f-person/git-blame.nvim",
   keys = {
-    { "<leader>??", "<cmd>GitBlameToggle<cr>", desc = "Toggle git blame" },
+    { "<leader>?", "<cmd>GitBlameToggle<cr>", desc = "which_key_ignore" },
+    { "<leader>gb", "<cmd>GitBlameToggle<cr>", desc = "Toggle git blame" },
   },
 }
