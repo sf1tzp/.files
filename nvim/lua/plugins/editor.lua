d0feae6033e07fa01abb3aa9087af1e40152d67b
Steven Fitzpatrick <steven@fitz.gg>
2023-04-11 04:42:01 -0500

    Migrate  keybinding to open files

--- nvim/lua/plugins/editor.lua
+++ nvim/lua/plugins/editor.lua
@@ -1,6 +1,11 @@
+local Util = require("lazyvim.util")
+
 return {
   {
     "telescope.nvim",
+    keys = {
+      { "<leader>oo", Util.telescope("files"), desc = "Find Files (root dir)" },
+    },
     dependencies = {
       "nvim-telescope/telescope-fzf-native.nvim",
       build = "make",
