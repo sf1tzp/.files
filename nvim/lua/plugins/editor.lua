4d485cf2a63348c11503dc69d781b705a5615757
Steven Fitzpatrick <steven@fitz.gg>
2023-04-14 12:15:36 -0500

    Add treesitter-current-functions

--- nvim/lua/plugins/editor.lua
+++ nvim/lua/plugins/editor.lua
@@ -4,14 +4,21 @@ return {
   {
     "telescope.nvim",
     keys = {
+      -- silent alternative for find files that I'm used to
       { "<leader>oo", Util.telescope("files"), desc = "which_key_ignore" },
+      -- keymaps for treesitter current functions
+      { "<leader>c@", "<cmd>GetCurrentFunctions<cr>", desc = "Find Functions" },
+      { "<leader>@", "<cmd>GetCurrentFunctions<cr>", desc = "which_key_ignore" },
     },
     dependencies = {
-      "nvim-telescope/telescope-fzf-native.nvim",
-      build = "make",
-      config = function()
-        require("telescope").load_extension("fzf")
-      end,
+      {
+        "nvim-telescope/telescope-fzf-native.nvim",
+        build = "make",
+        config = function()
+          require("telescope").load_extension("fzf")
+        end,
+      },
+      { "eckon/treesitter-current-functions" },
     },
   },
 }
