464b31cd2ff71bf8a7bbc1ea8bcb5ad426590239
Steven Fitzpatrick <steven@fitz.gg>
2023-04-14 16:07:00 -0500

    Fix harpoon on initial startup

--- nvim/lua/plugins/harpoon.lua
+++ nvim/lua/plugins/harpoon.lua
@@ -6,149 +6,40 @@ return {
     "folke/which-key.nvim",
   },
 
-  keys = function()
-    local ui = require("harpoon.ui")
-    local mark = require("harpoon.mark")
-
+  config = function()
+    require("harpoon").setup()
+    -- Add a section to which-key for harpoon
     local wk = require("which-key")
     wk.register({
       ["<leader>h"] = { name = "+harpoon" },
     })
-
-    return {
-      { "<leader>ha", mark.add_file, desc = "Harpoon: Mark File" },
-      { "<leader>hr", mark.rm_file, desc = "Harpoon: Remove File" },
-      { "<leader>hs", ui.toggle_quick_menu, desc = "Harpoon: Show Files" },
-      { "<leader>hh", ui.toggle_quick_menu, desc = "Harpoon: Show Files" },
-
-      -- Keybindings to jump between files 1-9
-      {
-        "<leader>h1",
-        function()
-          require("harpoon.ui").nav_file(1)
-        end,
-        desc = "Open File 1",
-      },
-      {
-        "<leader>h2",
-        function()
-          require("harpoon.ui").nav_file(2)
-        end,
-        desc = "Open File 2",
-      },
-      {
-        "<leader>h3",
-        function()
-          require("harpoon.ui").nav_file(3)
-        end,
-        desc = "Open File 3",
-      },
-      {
-        "<leader>h4",
-        function()
-          require("harpoon.ui").nav_file(4)
-        end,
-        desc = "Open File 4",
-      },
-      {
-        "<leader>h5",
-        function()
-          require("harpoon.ui").nav_file(5)
-        end,
-        desc = "Open File 5",
-      },
-      {
-        "<leader>h6",
-        function()
-          require("harpoon.ui").nav_file(6)
-        end,
-        desc = "Open File 6",
-      },
-      {
-        "<leader>h7",
-        function()
-          require("harpoon.ui").nav_file(7)
-        end,
-        desc = "Open File 7",
-      },
-      {
-        "<leader>h8",
-        function()
-          require("harpoon.ui").nav_file(8)
-        end,
-        desc = "Open File 8",
-      },
-      {
-        "<leader>h9",
-        function()
-          require("harpoon.ui").nav_file(9)
-        end,
-        desc = "Open File 9",
-      },
-      -- Silent Keybindings of the same
-      {
-        "<leader>1",
-        function()
-          require("harpoon.ui").nav_file(1)
-        end,
-        desc = "which_key_ignore",
-      },
-      {
-        "<leader>2",
-        function()
-          require("harpoon.ui").nav_file(2)
-        end,
-        desc = "which_key_ignore",
-      },
-      {
-        "<leader>3",
-        function()
-          require("harpoon.ui").nav_file(3)
-        end,
-        desc = "which_key_ignore",
-      },
-      {
-        "<leader>4",
-        function()
-          require("harpoon.ui").nav_file(4)
-        end,
-        desc = "which_key_ignore",
-      },
-      {
-        "<leader>5",
-        function()
-          require("harpoon.ui").nav_file(5)
-        end,
-        desc = "which_key_ignore",
-      },
-      {
-        "<leader>6",
-        function()
-          require("harpoon.ui").nav_file(6)
-        end,
-        desc = "which_key_ignore",
-      },
-      {
-        "<leader>7",
-        function()
-          require("harpoon.ui").nav_file(7)
-        end,
-        desc = "which_key_ignore",
-      },
-      {
-        "<leader>8",
-        function()
-          require("harpoon.ui").nav_file(8)
-        end,
-        desc = "which_key_ignore",
-      },
-      {
-        "<leader>9",
-        function()
-          require("harpoon.ui").nav_file(9)
-        end,
-        desc = "which_key_ignore",
-      },
-    }
   end,
+
+  keys = {
+    { "<leader>ha", "<cmd>lua require('harpoon.mark').add_file()<cr>", desc = "Harpoon: Mark File" },
+    { "<leader>hr", "<cmd>lua require('harpoon.mark').rm_file()<cr>", desc = "Harpoon: Remove File" },
+    { "<leader>hs", "<cmd>lua require('harpoon.ui').toggle_quick_menu()<cr>", desc = "Harpoon: Show Files" },
+    { "<leader>hh", "<cmd>lua require('harpoon.ui').toggle_quick_menu()<cr>", desc = "Harpoon: Show Files" },
+
+    -- Keybindings to jump between files 1-9
+    { "<leader>h1", "<cmd>lua require('harpoon.ui').nav_file(1)<cr>", desc = "Open File 1" },
+    { "<leader>h2", "<cmd>lua require('harpoon.ui').nav_file(2)<cr>", desc = "Open File 2" },
+    { "<leader>h3", "<cmd>lua require('harpoon.ui;).nav_file(3)<cr>", desc = "Open File 3" },
+    { "<leader>h4", "<cmd>lua require('harpoon.ui').nav_file(4)<cr>", desc = "Open File 4" },
+    { "<leader>h5", "<cmd>lua require('harpoon.ui').nav_file(5)<cr>", desc = "Open File 5" },
+    { "<leader>h6", "<cmd>lua require('harpoon.ui').nav_file(6)<cr>", desc = "Open File 6" },
+    { "<leader>h7", "<cmd>lua require('harpoon.ui').nav_file(7)<cr>", desc = "Open File 7" },
+    { "<leader>h8", "<cmd>lua require('harpoon.ui').nav_file(8)<cr>", desc = "Open File 8" },
+    { "<leader>h9", "<cmd>lua require('harpoon.ui').nav_file(9)<cr>", desc = "Open File 9" },
+    -- Silent Keybindings of the same
+    { "<leader>1", "<cmd>lua require('harpoon.ui').nav_file(1)<cr>", desc = "which_key_ignore" },
+    { "<leader>2", "<cmd>lua require('harpoon.ui').nav_file(2)<cr>", desc = "which_key_ignore" },
+    { "<leader>3", "<cmd>lua require('harpoon.ui;).nav_file(3)<cr>", desc = "which_key_ignore" },
+    { "<leader>4", "<cmd>lua require('harpoon.ui').nav_file(4)<cr>", desc = "which_key_ignore" },
+    { "<leader>5", "<cmd>lua require('harpoon.ui').nav_file(5)<cr>", desc = "which_key_ignore" },
+    { "<leader>6", "<cmd>lua require('harpoon.ui').nav_file(6)<cr>", desc = "which_key_ignore" },
+    { "<leader>7", "<cmd>lua require('harpoon.ui').nav_file(7)<cr>", desc = "which_key_ignore" },
+    { "<leader>8", "<cmd>lua require('harpoon.ui').nav_file(8)<cr>", desc = "which_key_ignore" },
+    { "<leader>9", "<cmd>lua require('harpoon.ui').nav_file(9)<cr>", desc = "which_key_ignore" },
+  },
 }
