514e1533bf9296596b81d374a06f44e1a007c495
Steven Fitzpatrick <steven@fitz.gg>
2023-05-03 21:17:56 -0600

    Disable plugins when using VS Code

--- nvim/init.lua
+++ nvim/init.lua
@@ -1,2 +1,6 @@
--- bootstrap lazy.nvim, LazyVim and your plugins
-require("config.lazy")
+if vim.g.vscode then
+  print("Using empty init.lua for VS Code Extension")
+else
+  -- bootstrap lazy.nvim, LazyVim and your plugins
+  require("config.lazy")
+end
