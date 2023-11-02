599ace5442512a57386fda9b4a1195614a2f23b4
Steven Fitzpatrick <steven@fitz.gg>
2023-11-01 21:29:11 -0700

    Unbind move lines in vim

--- nvim/lua/config/keymaps.lua
+++ nvim/lua/config/keymaps.lua
@@ -3,3 +3,11 @@
 -- Add any additional keymaps here
 
 vim.keymap.set("n", "<leader>ul", "<cmd>set relativenumber!<cr>") -- toggle relativenumber
+
+-- Move Lines
+vim.keymap.set("n", "<A-j>", "")
+vim.keymap.set("n", "<A-k>", "")
+vim.keymap.set("i", "<A-j>", "")
+vim.keymap.set("i", "<A-k>", "")
+vim.keymap.set("v", "<A-j>", "")
+vim.keymap.set("v", "<A-k>", "")
