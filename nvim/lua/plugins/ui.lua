b9a031e17a585f181c3148ddf73eb7f546d6abff
Steven Fitzpatrick <steven@fitz.gg>
2023-11-20 23:36:03 -0800

    Update indent blankline

--- nvim/lua/plugins/ui.lua
+++ nvim/lua/plugins/ui.lua
@@ -56,24 +56,23 @@ return {
       local headerPadding = vim.fn.max({ 2, vim.fn.floor(vim.fn.winheight(0) * marginTopPercent) })
       vim.cmd([[highlight AzureBlue guifg=#0080FF gui=nocombine]])
       local heading = [[
- _____                                   
-|  _  |___ _ _ ___ ___                   
-|     |- _| | |  _| -_|                  
-|__|__|___|___|_| |___|                  
-                                          
-                                          
-    _____                 _              
-   |     |___ ___ ___ ___| |_ ___ ___    
-   |  |  | . | -_|  _| .'|  _| . |  _|   
-   |_____|  _|___|_| |__,|_| |___|_|     
-         |_|                             
-                                          
-                   _____                 
-                  |   | |___ _ _ _ _ ___ 
+ _____
+|  _  |___ _ _ ___ ___
+|     |- _| | |  _| -_|
+|__|__|___|___|_| |___|
+
+
+    _____                 _
+   |     |___ ___ ___ ___| |_ ___ ___
+   |  |  | . | -_|  _| .'|  _| . |  _|
+   |_____|  _|___|_| |__,|_| |___|_|
+         |_|
+
+                   _____
+                  |   | |___ _ _ _ _ ___
                   | | | | -_|_'_| | |_ -|
                   |_|___|___|_,_|___|___|
       ]]
-
       local dashboard = require("alpha.themes.dashboard")
       dashboard.opts = opts
       dashboard.section.header.val = vim.split(heading, "\n")
@@ -90,23 +89,8 @@ return {
   },
   {
     "lukas-reineke/indent-blankline.nvim",
-    opts = function(_, opts)
-      vim.opt.termguicolors = true
-      vim.cmd([[highlight IndentBlanklineIndent1 guibg=#323232 gui=nocombine]])
-      vim.cmd([[highlight IndentBlanklineIndent2 guibg=#413d3b gui=nocombine]])
-
-      opts.char = ""
-      opts.char_highlight_list = {
-        "IndentBlanklineIndent1",
-        "IndentBlanklineIndent2",
-      }
-      opts.space_char_highlight_list = {
-        "IndentBlanklineIndent1",
-        "IndentBlanklineIndent2",
-      }
-      opts.show_trailing_blankline_indent = false
-      return opts
-    end,
+    main = "ibl",
+    opts = {},
   },
   {
     "echasnovski/mini.indentscope",
