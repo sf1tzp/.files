eb51643160c99b6085b2fd9e90e47e0dbda6ef77
Steven Fitzpatrick <steven@fitz.gg>
2023-12-10 00:05:54 -0800

    Update for harpoon 2

--- nvim/lua/plugins/harpoon.lua
+++ nvim/lua/plugins/harpoon.lua
@@ -1,6 +1,7 @@
 return {
   "ThePrimeagen/harpoon",
   enabled = true,
+  branch = "harpoon2",
   dependencies = {
     "nvim-lua/plenary.nvim",
     "folke/which-key.nvim",
@@ -19,31 +20,153 @@ return {
     })
   end,
 
-  keys = {
-    { "<leader>ha", "<cmd>lua require('harpoon.mark').add_file()<cr>", desc = "Harpoon: Mark File" },
-    { "<leader>hr", "<cmd>lua require('harpoon.mark').rm_file()<cr>", desc = "Harpoon: Remove File" },
-    { "<leader>hs", "<cmd>lua require('harpoon.ui').toggle_quick_menu()<cr>", desc = "Harpoon: Show Files" },
-    { "<leader>hh", "<cmd>lua require('harpoon.ui').toggle_quick_menu()<cr>", desc = "Harpoon: Show Files" },
+  keys = function()
+    local harpoon = require("harpoon")
 
-    -- Keybindings to jump between files 1-9
-    { "<leader>h1", "<cmd>lua require('harpoon.ui').nav_file(1)<cr>", desc = "Open File 1" },
-    { "<leader>h2", "<cmd>lua require('harpoon.ui').nav_file(2)<cr>", desc = "Open File 2" },
-    { "<leader>h3", "<cmd>lua require('harpoon.ui').nav_file(3)<cr>", desc = "Open File 3" },
-    { "<leader>h4", "<cmd>lua require('harpoon.ui').nav_file(4)<cr>", desc = "Open File 4" },
-    { "<leader>h5", "<cmd>lua require('harpoon.ui').nav_file(5)<cr>", desc = "Open File 5" },
-    { "<leader>h6", "<cmd>lua require('harpoon.ui').nav_file(6)<cr>", desc = "Open File 6" },
-    { "<leader>h7", "<cmd>lua require('harpoon.ui').nav_file(7)<cr>", desc = "Open File 7" },
-    { "<leader>h8", "<cmd>lua require('harpoon.ui').nav_file(8)<cr>", desc = "Open File 8" },
-    { "<leader>h9", "<cmd>lua require('harpoon.ui').nav_file(9)<cr>", desc = "Open File 9" },
-    -- Silent Keybindings of the same
-    { "<leader>1", "<cmd>lua require('harpoon.ui').nav_file(1)<cr>", desc = "which_key_ignore" },
-    { "<leader>2", "<cmd>lua require('harpoon.ui').nav_file(2)<cr>", desc = "which_key_ignore" },
-    { "<leader>3", "<cmd>lua require('harpoon.ui').nav_file(3)<cr>", desc = "which_key_ignore" },
-    { "<leader>4", "<cmd>lua require('harpoon.ui').nav_file(4)<cr>", desc = "which_key_ignore" },
-    { "<leader>5", "<cmd>lua require('harpoon.ui').nav_file(5)<cr>", desc = "which_key_ignore" },
-    { "<leader>6", "<cmd>lua require('harpoon.ui').nav_file(6)<cr>", desc = "which_key_ignore" },
-    { "<leader>7", "<cmd>lua require('harpoon.ui').nav_file(7)<cr>", desc = "which_key_ignore" },
-    { "<leader>8", "<cmd>lua require('harpoon.ui').nav_file(8)<cr>", desc = "which_key_ignore" },
-    { "<leader>9", "<cmd>lua require('harpoon.ui').nav_file(9)<cr>", desc = "which_key_ignore" },
-  },
+    return {
+      {
+        "<leader>ha",
+        function()
+          harpoon:list():append()
+        end,
+        desc = "Harpoon: Mark File",
+      },
+      {
+        "<leader>hh",
+        function()
+          harpoon.ui:toggle_quick_menu(harpoon:list())
+        end,
+        desc = "Harpoon: Show Files",
+      },
+
+      -- Keybindings to jump between files 1-9
+      {
+        "<leader>h1",
+        function()
+          harpoon:list():select(1)
+        end,
+        desc = "Open File 1",
+      },
+      {
+        "<leader>h2",
+        function()
+          harpoon:list():select(2)
+        end,
+        desc = "Open File 2",
+      },
+      {
+        "<leader>h3",
+        function()
+          harpoon:list():select(3)
+        end,
+        desc = "Open File 3",
+      },
+      {
+        "<leader>h4",
+        function()
+          harpoon:list():select(4)
+        end,
+        desc = "Open File 4",
+      },
+      {
+        "<leader>h5",
+        function()
+          harpoon:list():select(5)
+        end,
+        desc = "Open File 5",
+      },
+      {
+        "<leader>h6",
+        function()
+          harpoon:list():select(6)
+        end,
+        desc = "Open File 6",
+      },
+      {
+        "<leader>h7",
+        function()
+          harpoon:list():select(7)
+        end,
+        desc = "Open File 7",
+      },
+      {
+        "<leader>h8",
+        function()
+          harpoon:list():select(8)
+        end,
+        desc = "Open File 8",
+      },
+      {
+        "<leader>h9",
+        function()
+          harpoon:list():select(9)
+        end,
+        desc = "Open File 9",
+      },
+      -- Silent Keybindings of the same
+      {
+        "<leader>1",
+        function()
+          harpoon:list():select(1)
+        end,
+        desc = "which_key_ignore",
+      },
+      {
+        "<leader>2",
+        function()
+          harpoon:list():select(2)
+        end,
+        desc = "which_key_ignore",
+      },
+      {
+        "<leader>3",
+        function()
+          harpoon:list():select(3)
+        end,
+        desc = "which_key_ignore",
+      },
+      {
+        "<leader>4",
+        function()
+          harpoon:list():select(4)
+        end,
+        desc = "which_key_ignore",
+      },
+      {
+        "<leader>5",
+        function()
+          harpoon:list():select(5)
+        end,
+        desc = "which_key_ignore",
+      },
+      {
+        "<leader>6",
+        function()
+          harpoon:list():select(6)
+        end,
+        desc = "which_key_ignore",
+      },
+      {
+        "<leader>7",
+        function()
+          harpoon:list():select(7)
+        end,
+        desc = "which_key_ignore",
+      },
+      {
+        "<leader>8",
+        function()
+          harpoon:list():select(8)
+        end,
+        desc = "which_key_ignore",
+      },
+      {
+        "<leader>9",
+        function()
+          harpoon:list():select(9)
+        end,
+        desc = "which_key_ignore",
+      },
+    }
+  end,
 }
