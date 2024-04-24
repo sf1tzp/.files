0bbae1d479a43c90eeec9bda14d4d12a4c3447bf
Steven Fitzpatrick <steven@fitz.gg>
2024-04-24 01:23:12 -0700

    Some nvim changes that were hanging around

--- nvim/init.lua
+++ nvim/init.lua
@@ -1,5 +1,6 @@
 if vim.g.vscode then
   print("Using empty init.lua for VS Code Extension")
+  require("config.keymaps")
 else
   -- bootstrap lazy.nvim, LazyVim and your plugins
   require("config.lazy")
--- nvim/lua/config/keymaps.lua
+++ nvim/lua/config/keymaps.lua
@@ -2,6 +2,7 @@
 -- Default keymaps that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/keymaps.lua
 -- Add any additional keymaps here
 
+print("loading keymaps")
 vim.keymap.set("n", "<leader>ul", "<cmd>set relativenumber!<cr>") -- toggle relativenumber
 
 -- Move Lines
@@ -11,3 +12,5 @@ vim.keymap.set("i", "<A-j>", "")
 vim.keymap.set("i", "<A-k>", "")
 vim.keymap.set("v", "<A-j>", "")
 vim.keymap.set("v", "<A-k>", "")
+
+vim.keymap.set("n", "s", "")
