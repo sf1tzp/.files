3a62509d7932ba50944a02d76cf7111cbfd83491
Steven Fitzpatrick <steven@fitz.gg>
2025-04-11 06:01:29 +0000

    re-enter buffers at the last location

--- nvim/lua/config/options.lua
+++ nvim/lua/config/options.lua
@@ -41,6 +41,17 @@ return function()
      end,
   })
 
+  -- Reopen files at their last position
+  vim.api.nvim_create_autocmd("BufReadPost", {
+    callback = function()
+      local mark = vim.api.nvim_buf_get_mark(0, '"')
+      local lcount = vim.api.nvim_buf_line_count(0)
+      if mark[1] > 0 and mark[1] <= lcount then
+        vim.api.nvim_win_set_cursor(0, mark)
+      end
+    end,
+  })
+
   -- Search
   vim.opt.ignorecase = true          -- Case insensitive searching
   vim.opt.smartcase = true           -- Case-sensitive if uppercase present
